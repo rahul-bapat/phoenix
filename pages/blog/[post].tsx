c390ef5f5a4c375a1e6811f0138b35cd85e693d4
Chandrashiggaon <145744085+Chandrashiggaon@users.noreply.github.com>
2023-10-07 03:51:21 +0530

    call analytics.js from post.tsx

--- pages/blog/[post].tsx
+++ pages/blog/[post].tsx
@@ -132,6 +132,7 @@ export default function BlogPost({ blogPost, page, pageUrl }: { blogPost: BlogPo
         </div>
       </div>
       <input type='hidden' value={post.analytics_link} name='analytics_link' id='analytics_link'></input>
+      <script src='/analytics.js'async/>
     </>
   );
 }
